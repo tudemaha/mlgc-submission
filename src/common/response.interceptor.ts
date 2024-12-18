8b2b7775b49ce9cbcfa86f9105f9776089e06891
tudemaha <gede_mahardika88@yahoo.co.id>
2024-12-18 11:35:51 +0800

    fix: change response body based on req method

--- src/common/response.interceptor.ts
+++ src/common/response.interceptor.ts
@@ -12,12 +12,22 @@ export class CustomResponseInterceptor implements NestInterceptor {
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
     return next.handle().pipe(
-      map((data) => ({
-        status: 'success',
-        message: 'Model is predicted successfully',
-        data,
-      })),
+      map((data) => {
+        if (request.method == 'POST') {
+          return {
+            status: 'success',
+            message: 'Model is predicted successfully',
+            data,
+          };
+        } else {
+          return {
+            status: 'success',
+            data,
+          };
+        }
+      }),
     );
   }
 }
