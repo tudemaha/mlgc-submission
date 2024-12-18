fb29edc1e43c711a2df99dcf69dab9c7a6ecb432
tudemaha <gede_mahardika88@yahoo.co.id>
2024-12-18 10:03:10 +0800

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { CustomResponseInterceptor } from './common/response.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new CustomResponseInterceptor());
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
